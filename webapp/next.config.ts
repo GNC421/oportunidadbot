68e07a4137bcfd07ab2d085cbb8f2be903981fc3
GNC421 <nunezcano.guillermo@gmail.com>
2026-08-31 14:25:19 +0200

    new log for env var

--- webapp/next.config.ts
+++ webapp/next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 
-console.info(
-  "[webapp] NEXT_PUBLIC_API_BASE_URL:",
-  process.env.NEXT_PUBLIC_API_BASE_URL ?? "not configured"
-);
-
 const nextConfig: NextConfig = {
   /* config options here */
 };
